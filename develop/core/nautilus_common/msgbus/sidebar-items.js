8b14c2ff413d44ef08708fa46fbccbb22e6c5287
cjdsellers <chris@cjdsellers.io>
2023-11-11 05:13:41 +0000

    Update gh-pages to output generated at a6e0cd7

--- develop/core/nautilus_common/msgbus/sidebar-items.js
+++ develop/core/nautilus_common/msgbus/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["MessageBus","Subscription"],"type":["Handler"]};
+window.SIDEBAR_ITEMS = {"struct":["MessageBus","Subscription"]};
--- develop/core/nautilus_common/sidebar-items.js
+++ develop/core/nautilus_common/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["clock","enums","ffi","generators","logging","msgbus","python","testing","timer"]};
+window.SIDEBAR_ITEMS = {"mod":["clock","enums","ffi","generators","handlers","logging","msgbus","python","testing","timer"]};
