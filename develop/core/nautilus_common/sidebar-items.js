67350e1355c2a9a6b1f444563d9b9abb88e77f7e
cjdsellers <chris@cjdsellers.io>
2023-11-24 04:48:31 +0000

    Update gh-pages to output generated at b003d97

--- develop/core/nautilus_common/sidebar-items.js
+++ develop/core/nautilus_common/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["clock","enums","ffi","generators","handlers","logging","msgbus","python","testing","timer"]};
+window.SIDEBAR_ITEMS = {"mod":["clock","enums","factories","ffi","generators","handlers","logging","msgbus","python","testing","timer"]};
