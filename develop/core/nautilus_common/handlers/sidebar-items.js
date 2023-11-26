398906f749f44610c51456ea3f9f6e60a4cce778
cjdsellers <chris@cjdsellers.io>
2023-11-26 12:04:39 +0000

    Update gh-pages to output generated at 39fa0dd

--- develop/core/nautilus_common/handlers/sidebar-items.js
+++ develop/core/nautilus_common/handlers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["dummy_callable"],"struct":["EventHandler","MessageHandler","PyCallableWrapper"]};
+window.SIDEBAR_ITEMS = {"fn":["dummy_callable"],"struct":["EventHandler","MessageHandler","PyCallableWrapper","SafeMessageCallback","SafeTimeEventCallback"]};
