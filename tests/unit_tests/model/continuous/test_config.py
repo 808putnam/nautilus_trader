284c5f4ff1bcee2b3697e1d7b6b371f6427e1dbc
George Hill <georgehill010@gmail.com>
2023-12-05 20:26:58 +0000

    rename parameters

--- tests/unit_tests/model/continuous/test_config.py
+++ tests/unit_tests/model/continuous/test_config.py
@@ -1,51 +1,46 @@
-import pandas as pd
-
-from nautilus_trader.model.continuous.parameters import RollParameters
 from nautilus_trader.model.continuous.cycle import RollCycle
+from nautilus_trader.nautilus_trader.model.continuous.config import FuturesChainConfig
 
-class TestRollParameters:
+
+class TestRollConfig:
     def setup(self):
-
-        self.params = RollParameters(
+        self.params = FuturesChainConfig(
             hold_cycle="FGHJKMNQUVXZ",
             priced_cycle="FGHJKMNQUVXZ",
             roll_offset=-45,
             expiry_offset=14,
             carry_offset=1,
         )
-    
+
     def test_to_dict(self):
-        
         # Arrange, Act
         values = self.params.to_dict()
-        
+
         # Assert
         assert values == {
-            'hold_cycle': RollCycle("FGHJKMNQUVXZ"),
-            'priced_cycle': RollCycle("FGHJKMNQUVXZ"),
-            'roll_offset': -45,
-            'expiry_offset': 14,
-            'carry_offset': 1,
+            "hold_cycle": RollCycle("FGHJKMNQUVXZ"),
+            "priced_cycle": RollCycle("FGHJKMNQUVXZ"),
+            "roll_offset": -45,
+            "expiry_offset": 14,
+            "carry_offset": 1,
         }
-        
+
     def test_from_dict(self):
-        
         # Arrange
         value = {
-            'hold_cycle': "FGHJKMNQUVXZ",
-            'priced_cycle': "FGHJKMNQUVXZ",
-            'roll_offset': -45,
-            'expiry_offset': 14,
-            'carry_offset': 1,
+            "hold_cycle": "FGHJKMNQUVXZ",
+            "priced_cycle": "FGHJKMNQUVXZ",
+            "roll_offset": -45,
+            "expiry_offset": 14,
+            "carry_offset": 1,
         }
-        
+
         # Act
-        parameters = RollParameters.from_dict(value)
-        
+        parameters = FuturesChainConfig.from_dict(value)
+
         # Assert
         assert parameters.hold_cycle == RollCycle("FGHJKMNQUVXZ")
         assert parameters.priced_cycle == RollCycle("FGHJKMNQUVXZ")
         assert parameters.roll_offset == -45
         assert parameters.expiry_offset == 14
         assert parameters.carry_offset == 1
-        
