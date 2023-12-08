1fc3d087de6834cec3600c672da06d594fa2adc4
cjdsellers <chris@cjdsellers.io>
2023-12-08 06:14:48 +0000

    Update gh-pages to output generated at bd06975

--- develop/core/nautilus_model/python/events/order/sidebar-items.js
+++ develop/core/nautilus_model/python/events/order/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["denied","emulated","filled","initialized","pending_update","rejected","released","submitted","triggered","updated"]};
+window.SIDEBAR_ITEMS = {"mod":["denied","emulated","filled","initialized","pending_cancel","pending_update","rejected","released","submitted","triggered","updated"]};
