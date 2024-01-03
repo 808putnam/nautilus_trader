a73cf224d0066ff987032528766edaa79a704a83
cjdsellers <chris@cjdsellers.io>
2024-01-03 09:25:04 +0000

    Update gh-pages to output generated at b33dd05

--- develop/core/nautilus_persistence/arrow/sidebar-items.js
+++ develop/core/nautilus_persistence/arrow/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DataStreamingError","EncodingError"],"fn":["extract_column"],"mod":["bar","delta","quote","trade"],"trait":["ArrowSchemaProvider","DecodeDataFromRecordBatch","DecodeFromRecordBatch","EncodeToRecordBatch","WriteStream"]};
+window.SIDEBAR_ITEMS = {"enum":["DataStreamingError","EncodingError"],"fn":["extract_column"],"mod":["bar","delta","depth","quote","trade"],"trait":["ArrowSchemaProvider","DecodeDataFromRecordBatch","DecodeFromRecordBatch","EncodeToRecordBatch","WriteStream"]};
