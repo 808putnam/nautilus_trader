d268f04f6ecceebf27cfe8915b775a94ae6986c1
George Hill <georgehill010@gmail.com>
2024-03-13 16:56:27 +0000

    add docstrings

--- nautilus_trader/continuous/config.py
+++ nautilus_trader/continuous/config.py
@@ -4,6 +4,8 @@ from msgspec import Meta
 from nautilus_trader.common.config import NautilusConfig
 from nautilus_trader.common.config import NonNegativeInt
 from nautilus_trader.model.data import BarType
+from nautilus_trader.model.identifiers import InstrumentId
+
 from pyfutures.continuous.contract_month import ContractMonth
 from pyfutures.continuous.cycle import RollCycle
 
@@ -14,12 +16,18 @@ NonPositiveInt = Annotated[int, Meta(le=0)]
 
 class RollConfig(NautilusConfig, frozen=True):
     """
-    hold_cycle: The contract cycle string we want to hold
-    priced_cycle: The contract cycle string of available prices
-    roll_offset: The day, relative to the expiry date, when we usually roll
-    carry_offset: The number of contracts forward or backwards defines carry in the priced roll cycle
-    approximate_expiry_offset: The offset, relative to the first of the contract month that the expiry date approximately occurs
+    Configuration for the rolls
+
+    Parameters
+    ----------
+    hold_cycle : RollCycle, The contract cycle string we want to hold
+    priced_cycle : RollCycle, The contract cycle string of available prices
+    roll_offset : NonPositiveInt, The day, relative to the expiry date, when we usually roll
+    carry_offset : Literal[1, -1], The number of contracts forward or backwards defines carry in the priced roll cycle
+    approximate_expiry_offset : NonNegativeInt, The offset, relative to the first of the contract month that the expiry date approximately occurs
     """
+
+    instrument_id: InstrumentId
     hold_cycle: RollCycle
     priced_cycle: RollCycle
     roll_offset: NonPositiveInt
@@ -27,7 +35,24 @@ class RollConfig(NautilusConfig, frozen=True):
     carry_offset: Literal[1, -1]
     skip_months: list[ContractMonth] | None = None
 
+
 class ContractChainConfig(NautilusConfig, frozen=True):
+    """
+    Configuration for the rolling of contracts
+
+    Parameters
+    ----------
+    bar_type : BarType
+        The bar type of the bars that execute the rolls of the chain
+    roll_config : RollConfig
+        The configuration for the rolls
+    raise_expired : bool, default True
+        If an exception is raised when the contract fails to roll before the contract's expiry date
+    ignore_expiry_date : bool, default False
+        If the expiry_date of the current contract should be ignored when attempting to roll
+    start_month : ContractMonth, optional
+        The chain's starting month to initialize to when started up 
+    """
     bar_type: BarType
     roll_config: RollConfig
     raise_expired: bool = True
